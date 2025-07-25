1b024ea6a6fe1510eeb255ccfe363094deabb8cf
fardinmoezi <fardinm2000@gmail.com>
2025-07-25 14:56:29 +0330

    add country managent functionality in admin panel

--- app/Livewire/Admin/Country/Index.php
+++ app/Livewire/Admin/Country/Index.php
@@ -2,12 +2,36 @@
 
 namespace App\Livewire\Admin\Country;
 
+use App\Models\Country;
 use Livewire\Component;
+use Illuminate\Support\Facades\Validator;
 
 class Index extends Component
 {
+
+    public $name;
+
+    public function submit($formData, Country $country)
+    {
+        $validator = Validator::make($formData, [
+            'name' => 'required|string|max:30',
+        ],
+            [
+                '*.required' => 'فیلد ضروری است',
+                '*.string' => '! فرمت اشتباه است',
+                '*.max' => 'حداکثر تعداد کارکترها:30'
+            ]);
+        $validator->validate();
+        $country->submit($formData);
+        $this->reset();
+        $this->dispatch('success','عملیات افزودن با موفقیت انجام شد!');
+    }
+
     public function render()
     {
-        return view('livewire.admin.country.index')->layout('layouts.admin.app');
+        $countries = Country::all();
+        return view('livewire.admin.country.index', [
+            'countries' => $countries,
+        ])->layout('layouts.admin.app');
     }
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    use HasFactory;
+
+    //protected $fillable = ['name'];
+    protected $guarded = [];
+
+    public function submit($formData)
+    {
+        Country::query()->create([
+            'name' => $formData['name']
+        ]);
+    }
 }
