f46ac5d2abf3c70ded5bf323dad1b4662a679f77
fardinmoezi <fardinm2000@gmail.com>
2025-08-12 22:03:17 +0330

    skaleton for some component done.

---
+++ app/Repositories/client/ClientAuthRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories\client;
+
+class ClientAuthRepository
+{
+
+}
---
+++ app/Repositories/client/ClientAuthRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories\client;
+
+interface ClientAuthRepositoryInterface
+{
+
+}
